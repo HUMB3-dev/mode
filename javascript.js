6d8fb8c51fb6e5df8bc7bbfa27dfa7a8d7699fcd
HUMB3-dev <75095362+HUMB3-dev@users.noreply.github.com>
2021-10-28 23:29:05 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+var nombre=prompt('Princesa introduzca su nombre sin apellidos');
+
+ function detectarVacaSleep (nombre){
+    
+
+     if (nombre== 'Yordano' || nombre=='yordano' ){
+          alert("el modo VACASLEEP sera activado 5 en min")
+          
+     }
+     else{
+        alert('comepinga igual que etecsa pon el nombre bien')
+    }
+ }
+
+ detectarVacaSleep(nombre);
